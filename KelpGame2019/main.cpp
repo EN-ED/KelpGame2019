0f5875cae31710552b8119754754862388096174
NakagawaYuki <kingwasheart@yahoo.co.jp>
2019-07-21 20:02:27 +0900

    プロジェクトの作成・環境設定 環境設定がうまく出来ているかのテスト

---
+++ KelpGame2019/main.cpp
@@ -0,0 +1,70 @@
+#include "EffekseerForDXLib.h"
+
+
+/// --------------------------------------------------------------------------------------------------
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	// ウィンドウサイズを決める
+	int winWidth = 1920;
+	int winHeight = 1080;
+	int bitColor = 32;
+
+
+#ifdef _DEBUG
+	SetOutApplicationLogValidFlag(TRUE);	// ログテキスト出力する
+#elif NDEBUG
+	SetOutApplicationLogValidFlag(FALSE);	// ログテキスト出力しない
+#endif
+
+
+	SetWindowText("KelpGame2019");					// メインウインドウのウインドウタイトルを変更する
+	SetBackgroundColor(0, 0, 0);			// 背景色を白に変更
+	ChangeWindowMode(TRUE);						// ウィンドウズモードにさせる
+	SetEnableXAudioFlag(TRUE);					// XAudioを使用するようにする
+
+
+	SetGraphMode(winWidth, winHeight, bitColor);			// 画面サイズ設定
+	GetDefaultState(&winWidth, &winHeight, &bitColor);		// デフォルトウィンドウ値を得る
+	SetWindowSize(winWidth, winHeight);						// デフォルトウィンドウサイズに合わせてゲームサイズを変更
+
+
+	// ＤＸライブラリ初期化処理
+	if (DxLib_Init() == -1)
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+
+
+	// Effekseerを初期化する。引数には画面に表示する最大パーティクル数を設定する。
+	if (Effkseer_Init(500) == -1)
+	{
+		DxLib_End();
+		return -1;
+	}
+
+
+	// フルスクリーンウインドウの切り替えでリソースが消えるのを防ぐ。
+	SetChangeScreenModeGraphicsSystemResetFlag(FALSE);
+
+
+	Effekseer_Set2DSetting(1920, 1080);	// 2Dエフェクトの最大範囲を設定
+
+
+	SetDrawScreen(DX_SCREEN_BACK);	// 背景描画
+	SetMouseDispFlag(FALSE);		// マウスカーソルを非表示にする
+
+	
+	// メインループ
+	while (!ScreenFlip() && !ProcessMessage() && !ClearDrawScreen())
+	{
+		break;
+	}
+
+
+	// 削除
+	Effkseer_End();		// Effekseerを終了する
+	DxLib_End();		// DXライブラリの後始末
+
+
+	return 0;
+}
