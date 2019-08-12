fd7b25a404948e8f90019a0b5af32202a554813e
NakagawaYuki <kingwasheart@yahoo.co.jp>
2019-08-12 23:55:29 +0900

    タイトル部分を同期

--- KelpGame2019/Title.cpp
+++ KelpGame2019/Title.cpp
@@ -5,8 +5,147 @@
 
 
 /// ---------------------------------------------------------------------------------------------------------------------------------------------------------
+void Title::SceneOneDraw()
+{
+	// スタート文字
+	SetDrawBlendMode(DX_BLENDMODE_ALPHA, m_sceneOneStartBlendCount);
+	DrawGraph(m_sceneSideDrawX + 960 - 200, 800 - 24, mD_sceneOneStart, true);
+	SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+}
+
+
+
+/// ---------------------------------------------------------------------------------------------------------------------------------------------------------
+void Title::SceneOneProcess()
+{
+	if (m_isBlendDownSwitch)
+	{
+		if (m_sceneOneStartBlendCount-- < 50)
+		{
+			m_isBlendDownSwitch = !m_isBlendDownSwitch;
+		}
+	}
+	else
+	{
+		if (m_sceneOneStartBlendCount++ > 200)
+		{
+			m_isBlendDownSwitch = !m_isBlendDownSwitch;
+		}
+	}
+
+
+	if (KeyData::Get(KEY_INPUT_NUMPADENTER) == 1)
+	{
+		m_sceneChange = Scene::two;
+		m_sceneTwoFontBigCount = 1.0f;
+
+		m_sceneChangeCount = 0;
+	}
+}
+
+
+
+/// ---------------------------------------------------------------------------------------------------------------------------------------------------------
+void Title::SceneTwoDraw()
+{
+	if (m_cursolArea == TwoCursolArea::start)
+	{
+		// スタート文字
+		DrawRotaGraph((m_sceneSideDrawX + m_sceneTwoDefaultSideDrawX) + 560 - 200, 400 - 24, m_sceneTwoFontBigCount, -0.2, mD_sceneTwoStart, true);
+
+
+		// 終了文字
+		SetDrawBlendMode(DX_BLENDMODE_ALPHA, 100);
+		DrawRotaGraph((m_sceneSideDrawX + m_sceneTwoDefaultSideDrawX) + 560 - 200, 600 - 24, 1.0, -0.2, mD_sceneTwoEnd, true);
+		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+	}
+	else if (m_cursolArea == TwoCursolArea::end)
+	{
+		// スタート文字
+		SetDrawBlendMode(DX_BLENDMODE_ALPHA, 100);
+		DrawRotaGraph((m_sceneSideDrawX + m_sceneTwoDefaultSideDrawX) + 560 - 200, 400 - 24, 1.0, -0.2, mD_sceneTwoStart, true);
+		SetDrawBlendMode(DX_BLENDMODE_NOBLEND, 0);
+
+
+		// 終了文字
+		DrawRotaGraph((m_sceneSideDrawX + m_sceneTwoDefaultSideDrawX) + 560 - 200, 600 - 24, m_sceneTwoFontBigCount, -0.2, mD_sceneTwoEnd, true);
+	}
+}
+
+
+
+/// ---------------------------------------------------------------------------------------------------------------------------------------------------------
+void Title::SceneTwoProcess()
+{
+	if (m_isFontBigDownSwitch)
+	{
+		if ((m_sceneTwoFontBigCount -= 0.003f) < 1.0f)
+		{
+			m_isFontBigDownSwitch = !m_isFontBigDownSwitch;
+		}
+	}
+	else
+	{
+		if ((m_sceneTwoFontBigCount += 0.005f) > 1.15f)
+		{
+			m_isFontBigDownSwitch = !m_isFontBigDownSwitch;
+		}
+	}
+
+
+	if (KeyData::Get(KEY_INPUT_UP) == 1)
+	{
+		m_cursolArea = TwoCursolArea::start;
+		m_sceneTwoFontBigCount = 1.0f;
+	}
+	else if (KeyData::Get(KEY_INPUT_DOWN) == 1)
+	{
+		m_cursolArea = TwoCursolArea::end;
+		m_sceneTwoFontBigCount = 1.0f;
+	}
+
+
+	if (KeyData::Get(KEY_INPUT_NUMPADENTER) == 1)
+	{
+		if (m_cursolArea == TwoCursolArea::start)
+		{
+			BASICPARAM::e_nowScene = ESceneNumber::GAME;
+		}
+		else if (m_cursolArea == TwoCursolArea::end)
+		{
+			m_sceneChange = Scene::one;
+			m_sceneOneStartBlendCount = 50;
+
+			m_sceneChangeCount = 0;
+		}
+	}
+}
+
+
+
+/// ---------------------------------------------------------------------------------------------------------------------------------------------------------
 Title::Title()
 {
+	mD_sceneOneStart = LoadGraph("media\\start_sceneOne.png");
+
+	m_sceneOneStartBlendCount = 50;
+	m_isBlendDownSwitch = false;
+
+	mD_sceneTwoStart = LoadGraph("media\\start_sceneTwo.png");
+	mD_sceneTwoEnd = LoadGraph("media\\end_sceneTwo.png");
+
+	m_cursolArea = TwoCursolArea::start;
+	m_sceneTwoFontBigCount = 1.0f;
+	m_isFontBigDownSwitch = false;
+
+	m_backGroundColor = 10;
+
+	m_sceneChange = Scene::one;
+
+	m_sceneChangeCount = m_sceneChangeMaxCount;
+
+	m_sceneSideDrawX = 0;
+
 	m_endFlag = false;
 }
 
@@ -15,6 +154,9 @@ Title::Title()
 /// ---------------------------------------------------------------------------------------------------------------------------------------------------------
 Title::~Title()
 {
+	if (mD_sceneTwoEnd != -1) DeleteGraph(mD_sceneTwoEnd);
+	if (mD_sceneTwoStart != -1) DeleteGraph(mD_sceneTwoStart);
+	if (mD_sceneOneStart != -1) DeleteGraph(mD_sceneOneStart);
 }
 
 
@@ -22,10 +164,18 @@ Title::~Title()
 /// ---------------------------------------------------------------------------------------------------------------------------------------------------------
 void Title::Draw()
 {
-	DrawFormatString(0, 0, GetColor(255, 255, 255), "title");	// 現在タイトルだと表示
+	// 背景
+	DrawBox(0, 0, 1920, 1080, GetColor(10, m_backGroundColor, 10), true);
 
 
-	DrawFormatString(800, 500, GetColor(255, 255, 255), "Zキーで初めてね");	// 遷移のためのキー表示
+	if (m_sceneChange == Scene::one)
+	{
+		SceneOneDraw();
+	}
+	else if (m_sceneChange == Scene::two)
+	{
+		SceneTwoDraw();
+	}
 }
 
 
@@ -33,9 +183,22 @@ void Title::Draw()
 /// ---------------------------------------------------------------------------------------------------------------------------------------------------------
 void Title::Process()
 {
-	// Zキーを押したら
-	if (KeyData::Get(KEY_INPUT_Z) == 1)
+	if (m_sceneChange == Scene::one)
 	{
-		BASICPARAM::e_nowScene = ESceneNumber::GAME;		// GAMEシーンへ移行
+		SceneOneProcess();
+		if (m_sceneChangeCount++ < m_sceneChangeMaxCount)
+		{
+			m_backGroundColor--;
+			m_sceneSideDrawX += 1920 / m_sceneChangeMaxCount;
+		}
+	}
+	else if (m_sceneChange == Scene::two)
+	{
+		SceneTwoProcess();
+		if (m_sceneChangeCount++ < m_sceneChangeMaxCount)
+		{
+			m_backGroundColor++;
+			m_sceneSideDrawX -= 1920 / m_sceneChangeMaxCount;
+		}
 	}
 }
--- KelpGame2019/Title.hpp
+++ KelpGame2019/Title.hpp
@@ -5,6 +5,47 @@
 class Title : public BaseScene
 {
 private:
+	int m_backGroundColor;		// 背景色
+
+	enum class Scene { one, two };		// シーン
+
+	Scene m_sceneChange;		// 現在のシーン
+
+	int m_sceneChangeCount;		// シーンの移動完了までカウント
+
+	const int m_sceneChangeMaxCount = 20;		// シーンの移動終了までのカウント値
+
+	int m_sceneSideDrawX;			// シーン移動時の横移動値（oneとtwoを両方描画する）
+
+	const int m_sceneTwoDefaultSideDrawX = 1920;		// シーン移動時の横最大値
+
+
+	/// シーン１だけに関する
+
+	int mD_sceneOneStart;		// 最初のスタート文字
+
+	int m_sceneOneStartBlendCount;		// 最初の文字の透過値
+	bool m_isBlendDownSwitch;			// 文字の透過値を下げるかどうか
+
+	void SceneOneDraw();		// シーン１の描画
+	void SceneOneProcess();		// シーン１のプロセス
+
+
+	/// シーン２だけに関する
+
+	int mD_sceneTwoStart;		// 2番目のスタート文字
+	int mD_sceneTwoEnd;			// 2番目の終了文字
+
+	enum class TwoCursolArea { start, end };		// カーソルの位置
+
+	TwoCursolArea m_cursolArea;			// カーソルの位置がどこにあるか
+
+	float m_sceneTwoFontBigCount;		// カーソルが当たっている文字のサイズ値
+	bool m_isFontBigDownSwitch;			// 文字のサイズ値を下げるかどうか
+
+	void SceneTwoDraw();			// シーン２の描画
+	void SceneTwoProcess();			// シーン２のプロセス
+
 
 
 public:
