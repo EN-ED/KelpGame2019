989750876ff0fba386868d4b38bf5a86f2f65ec0
YukiNakagawa <kingwasheart@yahoo.co.jp>
2019-11-29 11:19:17 +0900

    BackGroundの修正

--- KelpGame2019/BackGround.hpp
+++ KelpGame2019/BackGround.hpp
@@ -2,29 +2,34 @@
 #include "DxLib.h"
 
 
-
 /*
 ゲーム中の背景の本体
 */
 class BackGround
 {
 private:
+	/// 画像-----------------------------------
 	int mD_backGround;			// 背景画像
 	int mD_underGround;			// 床画像
 	
-	int m_areaX;
-	int m_speed;
+	int m_areaX;				// スクロールでの座標
+	int m_speed;				// スクロールの速度
 
 
 public:
+	/// @param (t_stageCorse) 何ステージなのか
 	BackGround(int t_stageCorse);
 	~BackGround();
 
 
+	// 描画
 	void Draw();
 
+	// 処理
 	void Process();
 
-	void SetSpeed(const float& t_speed);
+	// キャラクターの速度からスクロールスピードを設定
+	// @param (t_speed) キャラクターの速度
+	void SetSpeed(const float t_speed);
 };
 
